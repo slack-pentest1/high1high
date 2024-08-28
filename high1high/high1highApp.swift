3a1274c11a8a75f94532ce0d9ead27e8c9e92ed7
slack-pentest1 <slack-pentest1@Blueinfy.net>
2024-08-28 12:35:00 +0530

    Initial Commit

---
+++ high1high/high1highApp.swift
@@ -0,0 +1,17 @@
+//
+//  high1highApp.swift
+//  high1high
+//
+//  Created by Test Ios on 28/08/24.
+//
+
+import SwiftUI
+
+@main
+struct high1highApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ high1highUITests/high1highUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  high1highUITestsLaunchTests.swift
+//  high1highUITests
+//
+//  Created by Test Ios on 28/08/24.
+//
+
+import XCTest
+
+final class high1highUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
